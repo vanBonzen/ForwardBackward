d190228aa6bdf95cb495564073aa6636aa4512c2
vanBonzen <Neuheiser@me.com>
2019-09-28 18:41:32 +0200

    initial commit

---
+++ ForwardBackward/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ForwardBackward.Tools;
+
+namespace ForwardBackward
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Program.ForwardBackward(100);
+            Program.ForwardBackward(100000);
+        }
+
+        private static void ForwardBackward(int iterations)
+        {
+            int result = 0;
+            result = Program.RollXTimes(iterations);
+            Program.OutputResult(result, iterations);
+        }
+        
+        private static int RollXTimes(int rolls)
+        {
+            Dice dice = Dice.Instance;
+            int result = 0;
+            
+            for (var i = 0; i < rolls; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result += dice.Roll;
+                }
+                else
+                {
+                    result -= dice.Roll;
+                }
+            }
+            return result;
+        }
+        
+        private static void OutputResult(int result, int numberOfRolls)
+        {
+            Console.Write("Position at " + numberOfRolls + " rolls: ");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ForwardBackward/Tools/Dice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ForwardBackward.Tools
+{
+    public class Dice
+    {
+        private static Dice _instance;
+        private static readonly object Padlock = new object();
+        
+        public int Roll
+        {
+            get
+            {
+                Random rnd = new Random();
+
+                return rnd.Next(1, 6);
+            }
+        }
+
+        private Dice()
+        {
+            
+        }
+
+        public static Dice Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Padlock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Dice();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
